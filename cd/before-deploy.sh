536d4e7700dbfb230ee9f45339170a2998e89a8d
tnsasse <tobias.sasse@de.ibm.com>
2017-04-02 12:42:20 +0200

    debugging travis ci scipts

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    ls -all
     openssl aes-256-cbc -K $encrypted_e3bc588545dd_key -iv $encrypted_e3bc588545dd_iv -in codesigning.asc.enc -out codesigning.asc -d    
     gpg --fast-import signingkey.asc
 fi

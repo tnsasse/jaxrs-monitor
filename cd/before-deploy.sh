c87350b86b3a702bba5fbdd6792ec8ef7bec6fa6
tnsasse <tobias.sasse@de.ibm.com>
2017-04-02 12:19:44 +0200

    fixed script pointing to wrong path

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_e3bc588545dd_key -iv $encrypted_e3bc588545dd_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d    
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_e3bc588545dd_key -iv $encrypted_e3bc588545dd_iv -in codesigning.asc.enc -out codesigning.asc -d    
+    gpg --fast-import signingkey.asc
 fi

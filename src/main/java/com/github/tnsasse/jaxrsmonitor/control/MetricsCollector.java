67c6ae7ed7769d5f056e1fd8cebf998268cec1a2
Tobias N. Sasse <tobi@byte23.de>
2017-04-06 18:55:50 +0200

    Added configuration facilities to restrict metrics exposed and path monitoring depth

--- src/main/java/com/github/tnsasse/jaxrsmonitor/control/MetricsCollector.java
+++ src/main/java/com/github/tnsasse/jaxrsmonitor/control/MetricsCollector.java
@@ -1,5 +1,10 @@
 package com.github.tnsasse.jaxrsmonitor.control;
 
+import com.github.tnsasse.jaxrsmonitor.entity.AppMetrics;
+import com.github.tnsasse.jaxrsmonitor.entity.NamedIntSummaryStatistics;
+import com.github.tnsasse.jaxrsmonitor.entity.ResponseMetrics;
+import com.github.tnsasse.jaxrsmonitor.entity.SystemMetrics;
+
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
@@ -8,13 +13,10 @@ import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import com.github.tnsasse.jaxrsmonitor.entity.AppMetrics;
-import com.github.tnsasse.jaxrsmonitor.entity.NamedIntSummaryStatistics;
-import com.github.tnsasse.jaxrsmonitor.entity.ResponseMetrics;
-import com.github.tnsasse.jaxrsmonitor.entity.SystemMetrics;
 
 /**
  * Gathers metrics about the application and runtime
@@ -25,68 +27,68 @@ import com.github.tnsasse.jaxrsmonitor.entity.SystemMetrics;
 @Singleton
 public class MetricsCollector {
 
-    private ZonedDateTime startupTime;
-    private MemoryMXBean memoryBean;
-    private OperatingSystemMXBean osBean;
+  private ZonedDateTime startupTime;
+  private MemoryMXBean memoryBean;
+  private OperatingSystemMXBean osBean;
 
-    private Map<String, NamedIntSummaryStatistics> responseTimesByPath;
+  private Map<String, NamedIntSummaryStatistics> responseTimesByPath;
 
-    @PostConstruct
-    public void init() {
-        this.startupTime = ZonedDateTime.now();
-        this.memoryBean = ManagementFactory.getMemoryMXBean();
-        this.osBean = ManagementFactory.getOperatingSystemMXBean();
-        this.responseTimesByPath = new HashMap<>();
+  @PostConstruct
+  public void init() {
+    this.startupTime = ZonedDateTime.now();
+    this.memoryBean = ManagementFactory.getMemoryMXBean();
+    this.osBean = ManagementFactory.getOperatingSystemMXBean();
+    this.responseTimesByPath = new HashMap<>();
+  }
+
+  public SystemMetrics getSystemMetrics() {
+    return new SystemMetrics(getCores(), getAvailableMemory(), getUsedMemory(), getLoadAverage());
+  }
+
+  public AppMetrics getAppMetrics() {
+    return new AppMetrics(getStartupTime(), getUptime());
+  }
+
+  public ResponseMetrics getResponseMetrics() {
+    return new ResponseMetrics(this.responseTimesByPath);
+  }
+
+  public void countResponseTime(final String httpPath, final String httpMethod, final Integer responseTime) {
+    if (!this.responseTimesByPath.containsKey(httpPath)) {
+      this.responseTimesByPath.put(httpPath, new NamedIntSummaryStatistics());
     }
 
-    public SystemMetrics getSystemMetrics() {
-        return new SystemMetrics(getCores(), getAvailableMemory(), getUsedMemory(), getLoadAverage());
-    }
+    this.responseTimesByPath.get(httpPath).addDataPoint(httpMethod, responseTime);
+  }
 
-    public AppMetrics getAppMetrics() {
-        return new AppMetrics(getStartupTime(), getUptime());
-    }
+  private ZonedDateTime getStartupTime() {
+    return this.startupTime;
+  }
 
-    public ResponseMetrics getResponseMetrics() {
-        return new ResponseMetrics(this.responseTimesByPath);
-    }
+  private long getUptime() {
+    return ChronoUnit.MILLIS.between(getStartupTime(), ZonedDateTime.now());
+  }
 
-    public void countResponseTime(final String httpPath, final String httpMethod, final Integer responseTime) {
-        if (!this.responseTimesByPath.containsKey(httpPath)) {
-            this.responseTimesByPath.put(httpPath, new NamedIntSummaryStatistics());
-        }
+  private long getAvailableMemory() {
+    MemoryUsage currentUsage = this.memoryBean.getHeapMemoryUsage();
+    long available = (currentUsage.getCommitted() - currentUsage.getUsed());
+    return toMegabyte(available);
+  }
 
-        this.responseTimesByPath.get(httpPath).addDataPoint(httpMethod, responseTime);
-    }
+  private long getUsedMemory() {
+    MemoryUsage currentUsage = this.memoryBean.getHeapMemoryUsage();
+    return toMegabyte(currentUsage.getUsed());
+  }
 
-    private ZonedDateTime getStartupTime() {
-        return this.startupTime;
-    }
+  private int getCores() {
+    return osBean.getAvailableProcessors();
+  }
 
-    private long getUptime() {
-        return ChronoUnit.MILLIS.between(getStartupTime(), ZonedDateTime.now());
-    }
+  private double getLoadAverage() {
+    return osBean.getSystemLoadAverage();
+  }
 
-    private long getAvailableMemory() {
-        MemoryUsage currentUsage = this.memoryBean.getHeapMemoryUsage();
-        long available = (currentUsage.getCommitted() - currentUsage.getUsed());
-        return toMegabyte(available);
-    }
-
-    private long getUsedMemory() {
-        MemoryUsage currentUsage = this.memoryBean.getHeapMemoryUsage();
-        return toMegabyte(currentUsage.getUsed());
-    }
-
-    private int getCores() {
-        return osBean.getAvailableProcessors();
-    }
-
-    private double getLoadAverage() {
-        return osBean.getSystemLoadAverage();
-    }
-
-    private long toMegabyte(final long bytes) {
-        return bytes / 1024 / 1024;
-    }
+  private long toMegabyte(final long bytes) {
+    return bytes / 1024 / 1024;
+  }
 }
